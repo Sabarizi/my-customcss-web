f129eabfa6fd01d173778624c0e9e385ce7e007b
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2025-01-10 16:10:30 +0500

    add

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,25 @@
+"use client"
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from '../styles/navbar.module.css';
 
 export default function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <h1 className={styles.logo}>My Website</h1>
-      <ul className={styles.navLinks}>
+      
+      <button 
+        className={styles.hamburger} 
+        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      >
+        ☰
+      </button>
+      
+      <ul 
+        className={`${styles.navLinks} ${isMobileMenuOpen ? styles.mobileMenu : ''}`}
+      >
         <li><Link href="/">Home</Link></li>
         <li><Link href="#Services">Services</Link></li>
         <li><Link href="#Contact">Contact</Link></li>
